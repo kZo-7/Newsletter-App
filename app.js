048fd75e295e85fca640f176f371511740b6f1c1
kZo-7 <kgkriz.bsn@gmail.com>
2023-03-31 23:44:47 +0300

    testing app.js with cyclic.sh - not working the same as localhost

--- app.js
+++ app.js
@@ -54,21 +54,20 @@ app.post("/", function (req, res) {
     const apiKey = process.env.API_KEY;
     const options = {
         method: "POST",
-        auth: `Sourdough:${process.env.API_KEY}`
+        auth: `Sourdough:${apiKey}`
     }
-    console.log("Api key = " + JSON.stringify(apiKey));
+    console.log("Api key = " + apiKey);
 
     //we are calling https.request() method to POST our data and see the response from mailchimp server
     const request = https.request(url, options, function (response) {
         console.log("Status Code : " + response.statusCode);
-        console.log("options : " + JSON.stringify(options.auth));
+        //console.log("options : " + JSON.stringify(options.auth));
         if (response.statusCode == 200) {
             res.sendFile(__dirname + "/success.html");
         } else {
             res.sendFile(__dirname + "/failure.html");
         }
 
-        console.log("[Into https.request] -> response.statusCode : " + response.statusCode);
         response.on("data", function (data) {
         // console.log("JSON.parse(data) = " + JSON.parse(data));
         })
@@ -89,5 +88,5 @@ app.post("/success", function (req, res) {
 });
 
 app.listen(PORT, function () {
-    console.log(`Server is running at port ${PORT}.`);
+    console.log(`Server is running at port ${JSON.stringify(PORT)}.`);
 });
